75f635650af08b896a153894c0fc785cdb2a1bbb
waleedarshad <waleed_arshad006@hotmail.com>
2022-11-17 09:14:54 +0500

    Revert "Fix merge errors"
    This reverts commit 7a36046a8343bff8b77a180ff4ce065d619524b1.

--- lib/users_list/users_provider.dart
+++ lib/users_list/users_provider.dart
@@ -12,8 +12,10 @@ class UsersProvider with ChangeNotifier {
   void fetchUsers() async {
     isUsersFetching = true;
     notifyListeners();
-    users = await _usersRepository.fetchUsersList();
+    users = await _usersRepository.getUserList();
     isUsersFetching = false;
+    users = await _usersRepository.fetchUsersList();
+    isLoading = false;
     notifyListeners();
   }
 }
